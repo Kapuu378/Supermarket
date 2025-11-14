24fb677b7d40c3b5238bee217ed9ccc246fa74e2
Francisco Gibert <kapuucompilations@gmail.com>
2025-11-14 15:08:35 -0300

    Added new module for detecting cluster ids in the Devoto API

---
+++ utils/find_cluster.py
@@ -0,0 +1,54 @@
+import requests
+import time
+import pickle
+import random
+import urllib3
+import os
+urllib3.disable_warnings()
+
+empty_cluster_ids = []
+valid_cluster_ids = []
+
+filepath = "/home/soporte/im5456448/Escritorio/supermarkets/utils"
+
+if os.path.exists(f"{filepath}/empty_cluster_ids.plk"):
+	with open(f"{filepath}/empty_cluster_ids.plk", "rb") as file:
+		empty_cluster_ids = pickle.load(file)
+
+if os.path.exists(f"{filepath}/valid_cluster_ids.plk"):
+	with open(f"{filepath}/valid_cluster_ids.plk", "rb") as file:
+		valid_cluster_ids = pickle.load(file)
+
+while True:
+	print(empty_cluster_ids, "       ", valid_cluster_ids)
+	try:
+		i = random.randint(3777, 15000)
+
+		if i in empty_cluster_ids:
+			continue
+
+		if i in valid_cluster_ids:
+			continue
+
+		print(f"Buscando {i}")
+
+		time.sleep(
+			random.randint(2,10)
+		)
+		res = requests.get(f"https://www.devoto.com.uy/api/catalog_system/pub/products/search?fq=productClusterIds:{i}", verify=False)
+
+		if len(res.content) < 3:
+			empty_cluster_ids.append(i)
+			continue
+
+		print(f"Encontrado ->{i}")
+		valid_cluster_ids.append(i)
+
+	except:
+		break
+
+with open("/home/soporte/im5456448/Escritorio/supermarkets/utils/valid_cluster_ids.plk", 'wb') as file:
+	pickle.dump(valid_cluster_ids, file)
+	
+with open("/home/soporte/im5456448/Escritorio/supermarkets/utils/empty_cluster_ids.plk", 'wb') as file:
+	pickle.dump(empty_cluster_ids, file)
